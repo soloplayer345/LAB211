968d96df4ecb6482bcdbd392474a1be5085db35e
Larry <90505134+soloplayer345@users.noreply.github.com>
2023-09-14 17:10:05 +0700

    up

--- Mytools/src/tools/Menu_module.java
+++ Mytools/src/tools/Menu_module.java
@@ -32,14 +32,17 @@ public class Menu_module {
         int choiceNo = Integer.parseInt(sc.nextLine());
         return (choiceNo >0 && choiceNo<=n) ? list.get(choiceNo-1): null;
     }
-    public static void main(String[] args) {
-        // Kiếm thử menu chứa các chuỗi trả về số int
-        ArrayList<String> strList = new ArrayList();
-        strList.add("Operation 1");
-        strList.add("Operation 2");
-        strList.add("Operation 3");
-        Menu_module Menu_module = new Menu_module();
-        
-    }
+//    public static void main(String[] args) {
+//        // Kiếm thử menu chứa các chuỗi trả về số int
+//        ArrayList<String> strList = new ArrayList();
+//        strList.add("Operation 1");
+//        strList.add("Operation 2");
+//        strList.add("Operation 3");
+//        Menu_module Menu_module = new Menu_module();
+//        int intChoice = Menu_module.getIntChoice(strList);
+//        System.out.println("Your choose: "+intChoice);
+//        //Kiểm thử menu chứa các ItemType trả về 1 ItemType
+//        ArrayList<ite>
+//    }
 }
 
